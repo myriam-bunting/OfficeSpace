968dfd02dad13c3fd0ed5ded12cb163aeeb31bf6
myriambunting <myriam.bunting@gmail.com>
2022-06-07 16:39:40 +0800

    start game almost complete

--- Project1/script.js
+++ Project1/script.js
@@ -102,6 +102,35 @@ const wholeDocument = async () => {
 
   const newPlayer = new Character();
 
+  // const startGame = () => {
+  //   const inputDiv = document.createElement("div");
+  //   inputDiv.className = "inputDiv";
+  //   inputDiv.innerText = `Enter your player name\n`;
+  //   document.querySelector(".gameBox").append(inputDiv);
+
+  //   const inputBar = document.createElement("input");
+  //   inputBar.className = "inputBar";
+  //   inputDiv.append(inputBar);
+
+  //   const typeDiv = document.createElement("div");
+  //   typeDiv.className = "typeDiv";
+  //   typeDiv.innerText = `Choose your player type\n`;
+  //   document.querySelector(".gameBox").append(typeDiv); // make 'create div' function
+
+  //   const playerSelection = () => {
+  //     for (type of charTypes) {
+  //       let selectType = document.createElement("button");
+  //       selectType.className = "playerTypeSelect";
+  //       selectType.setAttribute("id", `${type}`);
+  //       selectType.innerText = type;
+
+  //       document.querySelector(".typeDiv").append(selectType);
+  //     }
+  //   };
+  // };
+
+  // document.querySelector("#start-game").addEventListener("click", startGame);
+
   document.querySelector("#start-game").addEventListener("click", function () {
     const inputDiv = document.createElement("div");
     inputDiv.className = "inputDiv";
@@ -135,41 +164,35 @@ const wholeDocument = async () => {
         newPlayer.setName(inputBar.value);
       }
     });
+
     const getCharTypes = () => {
       document
         .getElementById(charTypes[0])
         .addEventListener("click", function () {
           newPlayer.setType(charTypes[0]);
-          console.log(`${charTypes[0]} is clicked`);
+          logText(`${charTypes[0]} is clicked`);
         });
 
       document
         .getElementById(charTypes[1])
         .addEventListener("click", function () {
           newPlayer.setType(charTypes[1]);
-          console.log(`${charTypes[1]} is clicked`);
+          logText(`${charTypes[1]} is clicked`);
         });
       document
         .getElementById(charTypes[2])
         .addEventListener("click", function () {
           newPlayer.setType(charTypes[2]);
-          console.log(`${charTypes[2]} is clicked`);
+          logText(`${charTypes[2]} is clicked`);
         });
       document
         .getElementById(charTypes[3])
         .addEventListener("click", function () {
           newPlayer.setType(charTypes[3]);
-          console.log(`${charTypes[3]} is clicked`);
+          logText(`${charTypes[3]} is clicked`);
         });
     };
     getCharTypes();
-
-    console.log(newPlayer.type);
-    // document.querySelector("#start-game").addEventListener("click", function () {
-    // const roomDiv = document.createElement("div");
-    // roomDiv.className = "roomDesc";
-    // roomDiv.innerText = `Enter your player name\n`;
-    // document.querySelector(".gameBox").append(roomDiv);
   });
 
   //   const getCharTypes = () => {
@@ -191,15 +214,30 @@ const wholeDocument = async () => {
     "No way through here",
     "Ouch! There's a wall",
     "Pivot! Pivot!",
-    "Try another way",
+    "Nothing to see here",
   ];
 
   const move = (direction) => {
     const currentRoom = getRoomById(newPlayer.currentRoom);
+
     if (currentRoom[direction] === null) {
       logText(ouch[Math.floor(Math.random() * ouch.length)]);
     } else {
+      console.log(getRoomById(newPlayer.currentRoom).id);
+      document.querySelector("#insetbar").innerText = getRoomById(
+        newPlayer.currentRoom
+      ).location;
       newPlayer.currentRoom = currentRoom[direction];
+      console.log(getRoomById(newPlayer.currentRoom).id);
+    }
+  };
+
+  const displayDescription = () => {
+    logText(getRoomById(newPlayer.currentRoom).description);
+    const currentRoom = getRoomById(newPlayer.currentRoom);
+    if (currentRoom[description] === null) {
+      logText("This room is empty");
+    } else {
       console.log(currentRoom);
       document.querySelector("#insetbar").innerText = getRoomById(
         newPlayer.currentRoom
@@ -207,33 +245,23 @@ const wholeDocument = async () => {
     }
   };
 
-  // const description = (description) => {
-  //   logText(getRoomById(newPlayer.currentRoom).description);
-  //   const currentRoom = getRoomById(newPlayer.currentRoom);
-  //   if (currentRoom[direction] === null) {
-  //     logText(ouch[Math.floor(Math.random() * ouch.length)]);
-  //   } else {
-  //     newPlayer.currentRoom = currentRoom[direction];
-  //     console.log(currentRoom);
-  //     document.querySelector("#insetbar").innerText = getRoomById(
-  //       newPlayer.currentRoom
-  //     ).location;
-  //   }
-  // };
-
   document.querySelector("#btn-north").addEventListener("click", function () {
+    displayDescription();
     move("north");
   });
 
   document.querySelector("#btn-west").addEventListener("click", function () {
     move("west");
+    displayDescription();
   });
 
   document.querySelector("#btn-east").addEventListener("click", function () {
     move("east");
+    displayDescription();
   });
   document.querySelector("#btn-south").addEventListener("click", function () {
     move("south");
+    displayDescription();
   });
 };
 wholeDocument();
